67fd5f3106211cc00347b4dea12823b512a4cf15
Steven Griset <sgriset@sbcglobal.net>
2020-10-14 12:59:48 -0700

    Intial Commit
    Intial Commit of Hacky Hour sample programs for TensorRT

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# tensorrt-hacky-hour
- Applications for TensorRT Hacky Hour 
+# TensorRT Applications
+
+
+This alwaysAI applications set use TensorRT binaries to do the local infernecing on a NVIDIA Jetson device, these binaries can be found in the alwaysAI model catalog.  The model will start with TRT and end with the Jetson device name it should be run on, for example nano.  These binaries are the most efficient way to do infernecing on NVIDIA Jetson device achieving infernecing times of 9 milliseconds on some devices.  Currently alwaysAI support TensorRT binaries for Jetson Nano, TX2 and Xavier XM.    
+
+
+## Repo Programs
+| Folder                     	| Description                                                                                              	|
+|----------------------------	|----------------------------------------------------------------------------------------------------------	|
+| license-vehicle-detection   | Program detects vehicles and license plates, currently setup to work with Jetson Nano|
+| distance-between-hands 	    | Program detections distance between hands in meters, this application needs a Intel Realsense camera to run and is setup to work with Jetson Nano|
+
+
+## Setup
+
+This app requires an alwaysAI account. Head to the [Sign up page](https://www.alwaysai.co/dashboard) if you don't have an account yet. Follow the instructions to install the alwaysAI tools on your development machine.
+
+Next, create an empty project to be used with this app. When you clone this repo, you can run `aai app configure` within the repo directory and your new project will appear in the list.
+
+## Usage
+
+Once the alwaysAI tools are installed on your development machine (or edge device if developing directly on it) you can run the following CLI commands:
+
+To set up the target device & install path
+
+```
+aai app configure
+```
+
+To install the app to your target (make sure you use the aai app models add command to add the current TensorRT model)
+
+```
+aai app install
+```
+
+To start the app
+
+```
+aai app start
+```
---
+++ license-vehicle-detection/app.py
@@ -0,0 +1,69 @@
+import time
+import edgeiq
+"""
+Use object detection to detect objects in the frame in realtime. The
+types of objects detected can be changed by selecting different models.
+
+To change the computer vision model, follow this guide:
+https://dashboard.alwaysai.co/docs/application_development/changing_the_model.html
+
+To change the engine and accelerator, follow this guide:
+https://dashboard.alwaysai.co/docs/application_development/changing_the_engine_and_accelerator.html
+
+To install app dependencies in the runtime container, list them in the requirements.txt file.
+"""
+
+
+def main():
+    obj_detect = edgeiq.ObjectDetection(
+            "alwaysai/TRT_ssd_mobilenet_v1_coco_vehicle_license_nano")
+    obj_detect.load(engine=edgeiq.Engine.TENSOR_RT)
+
+    print("Loaded model:\n{}\n".format(obj_detect.model_id))
+    print("Engine: {}".format(obj_detect.engine))
+    print("Accelerator: {}\n".format(obj_detect.accelerator))
+    print("Labels:\n{}\n".format(obj_detect.labels))
+
+    fps = edgeiq.FPS()
+
+    try:
+        with edgeiq.FileVideoStream('front_short.mp4', play_realtime=True) as video_stream, \
+                edgeiq.Streamer() as streamer:
+            # Allow Webcam to warm up
+            time.sleep(2.0)
+            fps.start()
+
+            # loop detection
+            while video_stream.more():
+                frame = video_stream.read()
+                results = obj_detect.detect_objects(frame, confidence_level=0.3)
+                frame = edgeiq.markup_image(
+                        frame, results.predictions, colors=obj_detect.colors)
+
+                # Generate text to display on streamer
+                text = ["Model: {}".format(obj_detect.model_id)]
+                text.append(
+                        "Inference time: {:1.3f} s".format(results.duration))
+                text.append("Objects:")
+
+                for prediction in results.predictions:
+                    text.append("{}: {:2.2f}%".format(
+                        prediction.label, prediction.confidence * 100))
+
+                streamer.send_data(frame, text)
+
+                fps.update()
+
+                if streamer.check_exit():
+                    break
+
+    finally:
+        fps.stop()
+        print("elapsed time: {:.2f}".format(fps.get_elapsed_seconds()))
+        print("approx. FPS: {:.2f}".format(fps.compute_fps()))
+
+        print("Program Ending")
+
+
+if __name__ == "__main__":
+    main()
